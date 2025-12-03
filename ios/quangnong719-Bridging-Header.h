ea4e5a89f4afc6b16f2b45e68dfc96a93748c202
Nham Nguyen Duc <ducnham.it86@gmail.com>
2025-12-04 02:08:51 +0700

    Setup Local Notification

---
+++ ios/quangnong719-Bridging-Header.h
@@ -0,0 +1,9 @@
+//
+//  quangnong719-Bridging-Header.h
+//  quangnong719
+//
+//  Created by Nham Nguyen Duc on 4/12/25.
+//
+
+#import <RNNotifications.h>
+
--- ios/quangnong719/AppDelegate.swift
+++ ios/quangnong719/AppDelegate.swift
@@ -8,7 +8,7 @@ class AppDelegate: RCTAppDelegate {
   override func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
     self.moduleName = "QuangNong719"
     self.dependencyProvider = RCTAppDependencyProvider()
-
+    RNNotifications.startMonitorNotifications()
     // You can add your custom initial props in the dictionary below.
     // They will be passed down to the ViewController used by React Native.
     self.initialProps = [:]
@@ -19,7 +19,18 @@ class AppDelegate: RCTAppDelegate {
   override func sourceURL(for bridge: RCTBridge) -> URL? {
     self.bundleURL()
   }
+  override func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken withDeviceToken: Data) {
+    RNNotifications.didRegisterForRemoteNotifications(withDeviceToken: withDeviceToken)
+  }
 
+  override func application(_ application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: Error) {
+    RNNotifications.didFailToRegisterForRemoteNotificationsWithError(error)
+  }
+
+  override func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable: Any],
+                 fetchCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void) {
+    RNNotifications.didReceiveBackgroundNotification(userInfo, withCompletionHandler: completionHandler)
+  }
   override func bundleURL() -> URL? {
 #if DEBUG
     RCTBundleURLProvider.sharedSettings().jsBundleURL(forBundleRoot: "index")
